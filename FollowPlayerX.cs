40b81da6335a8eac362b1e74ae6608d0f4ef9ebb
yzh-game-engineer <133076059+yzh-game-engineer@users.noreply.github.com>
2025-11-04 16:00:24 +0800

    这3个是challenge1的
    主要是分清坐标轴，然后是分清lateupdate update 和 fixedupdate的使用场合

---
+++ FollowPlayerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(30, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
